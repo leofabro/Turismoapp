c1a0d8f390635d321bc5348ebc46cbc446528330
leofabro <149259483+leofabro@users.noreply.github.com>
2024-08-13 16:55:18 -0500

    Delete src directory

--- src/main/java/com/puce/turismo/Repository/ActividadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.puce.turismo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.puce.turismo.Model.Actividad;
-
-public interface ActividadRepository extends MongoRepository<Actividad, String>{
-
-}
