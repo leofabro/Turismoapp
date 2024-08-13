fc67ba16a8df43cade18b3748cbc346c1893d50f
leofabro <149259483+leofabro@users.noreply.github.com>
2024-08-13 16:52:38 -0500

    Delete src directory

--- src/main/java/com/puce/turismo/Repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.puce.turismo.Repository;
-
-import com.puce.turismo.Model.Usuario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-    Usuario findByUsernameAndPassword(String username, String password);
-}
